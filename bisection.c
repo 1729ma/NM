9042d3e2f77d8a356bf1a06bac0f1a32ffb06384
1729ma <166897218+1729ma@users.noreply.github.com>
2025-03-25 18:22:01 +0530

    Update bisection.c

--- bisection.c
+++ bisection.c
@@ -1,37 +1,36 @@
 #include <stdio.h>
 
-// Function to evaluate the given equation
 double f(double x) {
     return x*x*x - 2*x - 5;
 }
 
-// Function to implement the Bisection Method
+
 void f2(double a, double b, int maxit,double x) {
     
     int i = 1;
     
 printf("Enter the maximum number of iterations: ");
     scanf("%d", &maxit);
-    // Taking valid root interval from user
+    
     do {
         printf("Enter values for a and b: ");
         scanf("%lf %lf", &a, &b);
 
         if (f(a) * f(b) > 0) {
-            printf("Invalid interval! f(a) and f(b) must have opposite signs.\n");
+            printf("Invalid interval! \n");
         } else {
             printf("The roots lie between %lf and %lf\n", a, b);
             break;
         }
     } while (1);
 
-    // Bisection Method iterations
+
     while (i <= maxit) {
         x = (a + b) / 2.0;
 
         printf("Iteration %d: Approximate Root = %lf\n", i, x);
 
-        if (f(x) == 0.0) {  // Exact root found
+        if (f(x) == 0.0) { 
             break;
         } else if (f(x) * f(a) < 0) {
             b = x;
@@ -49,11 +48,7 @@ int main() {
     double a, b,x;
     int maxit;
 
-    // User input for maximum iterations
-    
-
-    // Call the bisection method function
-    f2(a, b, maxit,x);
+    f2(a,b,maxit,x);
 
     return 0;
 }
